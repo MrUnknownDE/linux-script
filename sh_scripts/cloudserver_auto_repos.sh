0268e9585e5a85e51a467c5ca18d22184a7cae0a
Johannes Krüger <me@johanneskr.de>
2021-03-15 13:31:07 +0100

    add cloudserver_auto_repos.sh

---
+++ sh_scripts/cloudserver_auto_repos.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+clear
+echo "################################################"
+echo "#                                              #"
+echo "#                                              #"
+echo "#                 Debian 10                    #"
+echo "#         automatically Repo edit              #"
+echo "#                                              #"
+echo "#                    by                        #"
+echo "#               johanneskr.de                  #"
+echo "#                                              #"
+echo "#                                              #"
+echo "################################################"
+echo ""
+echo ""
+echo "DE >"
+echo "Dieses Script startet gleich automatisch."
+echo ""
+echo "EN >"
+echo "This script will start automatically."
+sleep 5
+echo ""
+echo ""
+echo ""
+echo "LETS GO!"
+
+apt install curl wget apt-transport-https dirmngr -y
+echo "
+#------------------------------------------------------------------------------#
+#                   OFFICIAL DEBIAN REPOS                    
+#------------------------------------------------------------------------------#
+
+###### Debian Main Repos
+deb http://deb.debian.org/debian/ stable main contrib non-free
+deb-src http://deb.debian.org/debian/ stable main contrib non-free
+
+deb http://deb.debian.org/debian/ stable-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free
+
+deb http://deb.debian.org/debian-security stable/updates main
+deb-src http://deb.debian.org/debian-security stable/updates main
+
+deb http://ftp.debian.org/debian buster-backports main
+deb-src http://ftp.debian.org/debian buster-backports main" > /etc/apt/source.list
+
+echo "Have Fun with Debian 10 Official Repos :)"
