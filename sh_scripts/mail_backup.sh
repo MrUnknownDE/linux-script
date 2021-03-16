e780689f886c9307111e6edbc141078d7f7401ba
Johannes Krüger <me@johanneskr.de>
2021-03-16 14:21:44 +0100

    finish mail_backup.sh

--- sh_scripts/mail_backup.sh
+++ sh_scripts/mail_backup.sh
@@ -5,6 +5,6 @@ echo "for Hetzner StorageBox!"
 
 
 MAILCOW_BACKUP_LOCATION=/opt/mailcow-dockerized/Backup /opt/mailcow-dockerized/helper-scripts/backup_and_restore.sh backup all --delete-days 1
-rsync --progress -e 'ssh -p xxx' --recursive /opt/mailcow-dockerized/Backup u259807-sub2@u259807.your-storagebox.de:/
+rsync --progress -e 'ssh -p xxx' --recursive /opt/mailcow-dockerized/Backup uXXXX-subXXX@uXXXX.your-storagebox.de:./
 echo "Finish"
 done
