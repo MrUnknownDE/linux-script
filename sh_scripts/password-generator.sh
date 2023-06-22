b5f0d2160573ba82690ccbe265798cf820d9cfbf
MrUnknownDE <me@johanneskr.de>
2023-06-22 22:52:21 +0200

    other password mechanism

--- sh_scripts/password-generator.sh
+++ sh_scripts/password-generator.sh
@@ -1,43 +1,22 @@
 #!/usr/bin/env sh
 if [[ -f $(which uuencode 2>/dev/null) ]]
     then
-        GEN_PW_SSH=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_SSH_KEY=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-128
-        done)
-        GEN_PW_FTP=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_MYSQL=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_MAIL=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
+        GEN_PW_SSH=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_SSH_KEY=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w128 | head -n 1)
+        GEN_PW_FTP=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_MYSQL=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_MAIL=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
         echo "SSH-Password: $GEN_PW_SSH"
         echo "SSH-KEY-Password: $GEN_PW_SSH_KEY"
         echo "FTP User Password: $GEN_PW_FTP"
         echo "MYSQL User Password: $GEN_PW_MYSQL"
         echo "e-Mail User Password: $GEN_PW_MAIL"
     else
-        GEN_PW_SSH=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_SSH_KEY=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-128
-        done)
-        GEN_PW_FTP=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_MYSQL=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-        GEN_PW_MAIL=$(for ((n=0;n<1;n++))
-        do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-32
-        done)
-	    apt update && apt install sharutils
+        GEN_PW_SSH=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_SSH_KEY=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w128 | head -n 1)
+        GEN_PW_FTP=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_MYSQL=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
+        GEN_PW_MAIL=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n 1)
         echo "SSH-Password: $GEN_PW_SSH"
         echo "SSH-KEY-Password: $GEN_PW_SSH_KEY"
         echo "FTP User Password: $GEN_PW_FTP"
