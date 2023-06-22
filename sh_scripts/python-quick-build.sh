32b3efbde79ff7c49a0f7cccd49acbd5b9923ab9
MrUnknownDE <me@johanneskr.de>
2023-06-22 23:24:21 +0200

    update on current version tested on debian 12 vm

--- sh_scripts/python-quick-build.sh
+++ sh_scripts/python-quick-build.sh
@@ -11,7 +11,7 @@ echo "$VERSION"
 echo "################################################"
 echo "#                                              #"
 echo "#                                              #"
-echo "#                Python3.9.2                   #"
+echo "#                Python3.11.4                  #"
 echo "#                Quick Build                   #"
 echo "#                                              #"
 echo "#                    by                        #"
@@ -35,16 +35,16 @@ echo "LETS GO!"
 sudo apt update
 sudo apt -y upgrade
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
-wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz
-tar -xf Python-3.9.9.tgz
-cd Python-3.9.9
+wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz
+tar -xf Python-3.11.4.tgz
+cd Python-3.11.4
 ./configure --enable-optimizations
 make -j $CPUCORES
 sudo make altinstall
 echo ""
 echo "########################################"
 echo ""
-echo "Python3.9 is installed"
-echo "lets check this with: python3.9 --version"
+echo "Python3.11.4 is installed"
+echo "lets check this with: python3.11 --version"
 echo ""
 echo "########################################"
