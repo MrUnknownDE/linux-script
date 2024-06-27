15aec007e304a9d6989822790e044056d8d7e170
MrUnknownDE <me@johanneskr.de>
2024-06-27 22:33:56 +0200

    update python version

--- sh_scripts/python-quick-build.sh
+++ sh_scripts/python-quick-build.sh
@@ -35,16 +35,16 @@ echo "LETS GO!"
 sudo apt update
 sudo apt -y upgrade
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
-wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz
-tar -xf Python-3.11.4.tgz
-cd Python-3.11.4
+wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz
+tar -xf Python-3.13.0.tgz
+cd Python-3.13.0
 ./configure --enable-optimizations
 make -j $CPUCORES
 sudo make altinstall
 echo ""
 echo "########################################"
 echo ""
-echo "Python3.11.4 is installed"
-echo "lets check this with: python3.11 --version"
+echo "Python 3.13.0 is installed"
+echo "lets check this with: python3.12 --version"
 echo ""
 echo "########################################"
