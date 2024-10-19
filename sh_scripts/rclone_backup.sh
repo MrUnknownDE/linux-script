a8deadf71c643de58b510dbee9d87578645f6b74
MrUnknownDE <me@johanneskr.de>
2024-10-19 17:44:30 +0200

    add bluesky-node (dropped)

--- sh_scripts/rclone_backup.sh
+++ sh_scripts/rclone_backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Quell- und Zielverzeichnis
 BACKUP_SOURCE="/opt"
-BACKUP_DEST="/data/backup"
+BACKUP_DEST="/data/backup/opt_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+RCLONE_COPY_DIR="/data/backup/"
 ziel_verzeichnis="/"
 
 # Rclone-Konfigurationsname
@@ -11,7 +12,6 @@ rclone_konfig="web-opt-backup"
 telegram_bot_token="YOUR_TELEGRAM_BOT_TOKEN"
 telegram_chat_id="YOUR_CHAT_ID"
 
-# Funktion, um eine Nachricht an Telegram zu senden
 send_telegram_message() {
     local message=$1
     curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d text="$message"
@@ -25,48 +25,35 @@ if [ "$1" == "telegram-test" ]; then
     exit 0
 fi
 
-# Vorhandene backup.log-Datei löschen
-log_datei="backup.log"
-if [ -f "$log_datei" ]; then
-    echo "Lösche vorhandene backup.log-Datei..."
-    rm "$log_datei"
-fi
-
 # Backup durchführen
-if tar -czf $BACKUP_DEST $BACKUP_SOURCE; then
-        echo "Backup erfolgreich erstellt: $BACKUP_DEST"
-        send_telegram_message "[$TIMESTAMP] [$HOSTNAME] Backup erfolgreich erstellt: $BACKUP_DEST"
-    else
-        echo "Fehler beim Erstellen des Backups."
-        send_telegram_message "[$TIMESTAMP] [$HOSTNAME] Fehler beim Erstellen des Backups."
-fi
+tar -czf $BACKUP_DEST $BACKUP_SOURCE > backup.log 2>&1
 
-# Überprüfen, ob das Mailcow Backup erfolgreich war
+# Überprüfen, ob das OPT Backup erfolgreich war
 if [ $? -eq 0 ]; then
-    send_telegram_message "Erfolg!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: Mailcow-Backup"
-    echo "Mailcow Backup erfolgreich."
+    send_telegram_message "Erfolg!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: opt-Backup"
+    echo "Opt Backup erfolgreich."
 else
-    echo "Fehler beim Mailcow Backup."
+    echo "Fehler beim OPT Backup."
     error_message=$(tail -n 20 backup.log)
-    send_telegram_message "Fehlgeschlagen!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: Mailcow-Backup%0AError: $error_message"
-    echo "Fehler beim Mailcow Backup. Logs wurden gesendet."
+    send_telegram_message "Fehlgeschlagen!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: Opt-Backup%0AError: $error_message"
+    echo "Fehler beim OPT Backup. Logs wurden gesendet."
     exit 0
 fi
 
 # Rclone-Befehl zum Kopieren des Verzeichnisses
 echo "Die Daten werden jetzt auf den S3 Speicher hochgeladen"
 sleep "5"
-rclone copy "$quelle_verzeichnis" "$rclone_konfig":"$ziel_verzeichnis"
+rclone copy "$RCLONE_COPY_DIR" "$rclone_konfig":"$ziel_verzeichnis"
 
 # Überprüfen, ob der Upload erfolgreich war
 if [ $? -eq 0 ]; then
     echo "Backup erfolgreich auf S3 hochgeladen."
     # Telegram-Benachrichtigung senden
-    send_telegram_message "Erfolg!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: S3-Mailcow-Backup-Upload"
+    send_telegram_message "Erfolg!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: Hetzner-opt-Backup-Upload"
 else
     echo "Fehler beim Hochladen des Backups auf S3."
     # Telegram-Benachrichtigung senden
-    send_telegram_message "Fehlgeschlagen!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: S3-Mailcow-Backup-Upload"
+    send_telegram_message "Fehlgeschlagen!%0AServer: web.johanneskr.de%0ATime: $(date +"%Y-%m-%d %H:%M:%S")%0AApplication: Hetzner-opt-Backup-Upload"
     exit 0
 fi
 
